83dcecdf1bbaf0210e84033e7d085914aac490d1
jot <jotjotjotjotjot@gmail.com>
2020-05-23 15:01:43 +0100

    integrate tabbed sortable lists main page.

--- js/playlist.js
+++ js/playlist.js
@@ -1,4 +1,5 @@
 var lastTab = "";
+
 function openTab(tabName) {
 	if (tabName == lastTab) return;
 
@@ -20,88 +21,37 @@ function openTab(tabName) {
 }
 
 
-
-function TabButtonRect(l, r, t, b) {
-	this.left = l;
-	this.right = r;
-	this.top = t;
-	this.bottom = b;
-
-	this.contains = function (x, y) {
-		return this.left <= x && x <= this.right &&
-			   this.top <= y && y <= this.bottom;
-	}
-	
-	this.toString = function() {
-		return this.left + "\r\n" + this.right + "\r\n" + this.top + "\r\n" + this.bottom + "\r\n\r\n";
-	}
-	this.print = function() {
-		console.log(this.toString());
-	}
-}
-
-function getElementPageCoordinates(elem)
-{
-	var os = $(elem).offset();
-	var l = os.left;
-	var r = os.left + elem.scrollWidth; //https://stackoverflow.com/questions/10277323/get-real-width-of-elements-with-jquery
-	var t = os.top;
-	var b = os.top + elem.scrollHeight;
-
-	return new TabButtonRect(l, r, t, b);
-}
-
-let rectMap = new Map();
-var tabsRect;
-
-
 function tabTouchMoveEventsFn(e) {
-	//console.log("tab hover");
-	//console.log(e);
-	//document.getElementById("textarea1").value = e.touches[0].pageX + ", " + e.touches[0].pageY + "\r\n";
+	//https://stackoverflow.com/questions/3918842/how-to-find-out-the-actual-event-target-of-touchmove-javascript-event
 	var xcoord = e.touches[0].pageX;
 	var ycoord = e.touches[0].pageY;
 	var targetElement = document.elementFromPoint(xcoord, ycoord);
 	
-	var tabName;
 	if (targetElement) {
-		if (targetElement.classList.contains("tablinkicon")) {
-			targetElement = targetElement.parentElement;
-		}
+		var tabName;
 		
 		if (targetElement.classList.contains("tablinks")) {
 			tabName = $(targetElement).data("tabname");
 		}
-	}
-	if (!tabName) {
-		//document.getElementById("textarea1").value = "";
-		return;
-	}
-  
-	//document.getElementById("textarea1").value = tabName;
+		else if (targetElement.classList.contains("tablinkicon")) {
+			tabName = $(targetElement.parentElement).data("tabname");
+		}
 
-	console.log(tabName);
-	
-	openTab(tabName);
+		if (tabName) {
+			console.log(tabName);
+			openTab(tabName);
+		}
+	}
 }
-
 function turnOnTabTouchMoveEvents() {
-	//$("#tabsPanel").on("mouseover", "button", tabTouchMoveEventsFn );
-	//$("#tabsPanel").on("mouseover touchmove", "button", tabTouchMoveEventsFn );
-	//$("#tab").on("touchmove", ".tablinks", tabTouchMoveEventsFn );
-	//document.addEventListener('touchmove', tabTouchMoveEventsFn, false);
-
 	document.getElementById("tabsPanel").addEventListener('touchmove', tabTouchMoveEventsFn, false);
 }
 function turnOffTabTouchMoveEvents() {
-	//$("#tabsPanel").off("mouseover", "button", tabTouchMoveEventsFn );
-	//$("#tabsPanel").off("mouseover touchmove", "button", tabTouchMoveEventsFn );
-	//$("#tab").off("touchmove", ".tablinks", tabTouchMoveEventsFn );
-	//document.removeEventListener('touchmove', tabTouchMoveEventsFn, false);
-
 	document.getElementById("tabsPanel").removeEventListener('touchmove', tabTouchMoveEventsFn, false);
 }
 
+let sortablePlaylistsMap = new Map();
+let sortableTablist;
 				
 function InitialiseSortablePlaylist(tabSelector, playlistClass) {
 	if (tabSelector === undefined || playlistClass === undefined)
@@ -130,13 +80,11 @@ function InitialiseSortablePlaylist(tabSelector, playlistClass) {
 		
 		onStart: function (e) {
 			//https://github.com/SortableJS/Sortable/issues/1292
-			//turnOffTabEvents();
 			turnOnTabTouchMoveEvents();
 		},
 		onEnd: function (e) {
 			//https://github.com/SortableJS/Sortable/issues/1292
 			turnOffTabTouchMoveEvents();
-			//turnOnTabEvents();
 		},
 		onChoose: function (/**Event*/evt) {
 			$(".list-group").removeClass("hoverable-items");
@@ -146,10 +94,8 @@ function InitialiseSortablePlaylist(tabSelector, playlistClass) {
 		},
 	});
 	
-	var tabElem = document.getElementById(tabSelector + "-button");
-	var rect = getElementPageCoordinates(tabElem);
 
-	rectMap.set(tabSelector, [s, rect]);
+	sortablePlaylistsMap.set(tabSelector, s);
 	
 	console.log("Initialised sortable list.");
 	console.log("	tabSelector: "+tabSelector);
@@ -157,12 +103,11 @@ function InitialiseSortablePlaylist(tabSelector, playlistClass) {
 }
 
 function DestroySortablePlaylist(tabSelector) {
-	var val = rectMap.get(tabSelector);
-	if (!val) return;
-	var s = val[0];
+	var s = sortablePlaylistsMap.get(tabSelector);
+	if (!s) return;
 	var group = s.options.group.name;
 
-	rectMap.delete(tabSelector);
+	sortablePlaylistsMap.delete(tabSelector);
 	s.destroy();
 	
 	console.log("Destroyed sortable.");
@@ -170,7 +115,7 @@ function DestroySortablePlaylist(tabSelector) {
 	console.log("	group: "+group);
 }
 
-function DestroyAllSortablePlaylist() {
+function DestroyAllSortablePlaylists() {
 	console.log("Destroying all sortable lists.");
 	var tabButtonList = $(".tablinks");
 	
@@ -196,9 +141,9 @@ function setupPlaylistNamespace() {
 	window.playlistUI = playlistUI;
 }
 
-function tabEventsFn() {
-	console.log("click tab");
 
+function tabEventsFn() {
+	console.log("clicked tab");
 	var tabName = $(this).data("tabname");
 	openTab( tabName );
 	callbackTabButtons(tabName);
@@ -219,11 +164,9 @@ function InitialiseSortableTabs(tabSelector, group) {
 		return;
 	}
 	var listSelector = "#" + tabSelector;
-	
-	console.log(listSelector);
 	var listElem = $(listSelector).get()[0];
 
-	var s = Sortable.create(listElem, {
+	sortableTablist = Sortable.create(listElem, {
 		group: group, // set lists to same group
 		/*handle: '.glyphicon-move',*/
 		/*chosenClass: 'chosen-item',
@@ -234,8 +177,6 @@ function InitialiseSortableTabs(tabSelector, group) {
 		bubbleScroll: true,
 		scrollSensitivity: 40,
 		scrollSpeed: 10,
-		/*forceFallback: true,
-		fallbackOnBody: true,*/
 		onStart: function (e) {
 			console.log("start");
 			turnOffTabEvents();
@@ -256,12 +197,10 @@ function InitialiseSortableTabs(tabSelector, group) {
 */
 	});
 	
-	tabsRect = getElementPageCoordinates(tab);
 
 	turnOnTabEvents();
 	setupPlaylistNamespace();
 
-	
 	console.log("Initialised sortable tabs.");
 	console.log("	tabSelector: "+tabSelector);
 	console.log("	group: "+group);
